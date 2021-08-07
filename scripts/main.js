29bac6a460e5eba7c66257d6f5d67a811f001eba
esecevce <esecevce@gmail.com>
2021-08-07 20:53:46 +0300

    Clear the current grid and sent the user a popup asking for the number of squares per side for the new grid

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,35 @@
 const squares = document.querySelector("#squares");
 
-let num = 16;
-
-for (let i = 0; i < num * num; i++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
-    squares.appendChild(square);
-
-    square.addEventListener("mouseover", () => {
-        square.setAttribute("style", "background-color: red");
-    });
+function createGrid(num) {
+    for (let i = 0; i < num * num; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        squares.appendChild(square);
+    
+        square.addEventListener("mouseover", () => {
+            square.setAttribute("style", "background-color: red");
+        });
+    }
+    
+    squares.setAttribute("style", `grid-template-columns: repeat(${num}, calc(400px / ${num})); grid-template-rows: repeat(${num}, calc(400px / ${num}));`);
 }
+
+function removeChilds(parent) {
+    while (parent.lastChild) {
+        parent.removeChild(parent.lastChild);
+    }
+}
+
+btnClear = document.querySelector("#btn-clear");
+btnClear.addEventListener("click", () => {
+    removeChilds(squares);
+
+    let gridNum = prompt("Enter a number: ");
+    if (gridNum > 100 || gridNum < 0) {
+        gridNum = prompt("Invalid. Enter another number: ");
+    }
+    gridNum = Number(gridNum);
+    createGrid(gridNum);
+});
+
+createGrid(16);
